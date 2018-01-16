413877580b2444fe92624957fb9b25ccafda0635
Mateus de Paula Veloso <mateusveloso.developer@gmail.com>
2018-01-16 02:09:52 -0200

    Visualização do iventário

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
  * Basic example demonstrating passport-steam usage within Express framework
  */
 var express = require('express')
-  , passport = require('passport')
-  , util = require('util')
-  , session = require('express-session')
-  , SteamStrategy = require('./strategy.js') 
-  , path = require('path');
+        , passport = require('passport')
+        , util = require('util')
+        , session = require('express-session')
+        , SteamStrategy = require('./strategy.js')
+        , iven = require("steam-inventory")
+        , path = require('path');
 
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
@@ -15,12 +16,12 @@ var express = require('express')
 //   the user by ID when deserializing.  However, since this example does not
 //   have a database of user records, the complete Steam profile is serialized
 //   and deserialized.
-passport.serializeUser(function(user, done) {
-  done(null, user);
+passport.serializeUser(function (user, done) {
+    done(null, user);
 });
 
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
 });
 
 // Use the SteamStrategy within Passport.
@@ -31,21 +32,20 @@ passport.use(new SteamStrategy({
     returnURL: 'http://localhost:3000/auth/steam/return',
     realm: 'http://localhost:3000/',
     apiKey: '181EA201D7BE357D64DEDDEB1502C91D'
-  },
-  function(identifier, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
+},
+        function (identifier, profile, done) {
+            // asynchronous verification, for effect...
+            process.nextTick(function () {
 
-      // To keep the example simple, the user's Steam profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the Steam account with a user record in your database,
-      // and return that user instead.
-      profile.identifier = identifier;
-      return done(null, profile);
-    });
-  }
+                // To keep the example simple, the user's Steam profile is returned to
+                // represent the logged-in user.  In a typical application, you would want
+                // to associate the Steam account with a user record in your database,
+                // and return that user instead.
+                profile.identifier = identifier;
+                return done(null, profile);
+            });
+        }
 ));
-
 var app = express();
 
 // configure Express
@@ -54,8 +54,8 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-    secret: 'your secret',
-    name: 'name of session id',
+    secret: require('crypto').randomBytes(64).toString('hex'),
+    name: 'BuySkins Session',
     resave: true,
     saveUninitialized: true}));
 
@@ -65,17 +65,29 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(__dirname + '/../../public'));
 
-app.get('/', function(req, res){
-  res.render('index', { user: req.user });
+app.get('/', function (req, res) {
+    res.render('index', {user: req.user});
 });
 
-app.get('/account', ensureAuthenticated, function(req, res){
-  res.render('account', { user: req.user });
+app.get('/account', ensureAuthenticated, function (req, res) {
+    res.render('account', {user: req.user});
 });
 
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+app.get('/iventario', ensureAuthenticated,function (req, res){
+    if (req.user) {
+        iven(req.user.id, 730, 2, (items, error) => {
+            if (error) {
+                return console.log("Erro: Não foi possivel acessar o iventario do jogador : " + req.user.displayName);
+            } else {
+                res.render('iventario', {user: req.user, iventario: items});
+            }
+        });
+    }
 });
 
 // GET /auth/steam
@@ -84,21 +96,22 @@ app.get('/logout', function(req, res){
 //   the user to steamcommunity.com.  After authenticating, Steam will redirect the
 //   user back to this application at /auth/steam/return
 app.get('/auth/steam',
-  passport.authenticate('steam', { failureRedirect: '/' }),
-  function(req, res) {
-    res.redirect('/');
-  });
-
+        passport.authenticate('steam', {failureRedirect: '/'}),
+        function (req, res) {
+            res.redirect('/');
+        });
 // GET /auth/steam/return
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
 app.get('/auth/steam/return',
-  passport.authenticate('steam', { failureRedirect: '/' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+        passport.authenticate('steam', {failureRedirect: '/'}),
+        function (req, res) {
+            console.log('caiu aqui 2s');
+
+            res.redirect('/');
+        });
 
 app.listen(3000);
 
@@ -108,6 +121,12 @@ app.listen(3000);
 //   the request will proceed.  Otherwise, the user will be redirected to the
 //   login page.
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/');
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
 }
+
+//const market = require('steam-market-pricing');
+//market.getItemPrice(730, 'AK-47 | Point Disarray (Field-Tested)',[currency = 7]).then(item => console.log(item));
+
